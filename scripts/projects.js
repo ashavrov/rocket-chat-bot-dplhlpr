08986ed0254c8b88e15c3e935e144afef789a719
Alexsey Shavrov <shavrov.a.k@gmail.com>
2019-05-08 15:49:45 +0300

    проекты извлекаются из бд

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,9 +1,22 @@
+var sqlite3 = require('sqlite3').verbose();
+require('dotenv').config();
+
 module.exports = (robot) => {
   robot.hear(/^--projects$/gi, function(res) {
-    res.send(
-      `Список актуальных проектов:
-      1) e2e
-	    2) 19Q2`
-    )
+    var answer = "Список актуальных проектов:\r\n";
+
+    let sqlData = `SELECT name FROM projects`;
+    //извлекаем проекты
+    let db = new sqlite3.Database(process.env.DB_FILE);
+    db.all(sqlData, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      for (var i = 0; i < rows.length; i++) {
+        answer += (i + 1) + ") " + rows[i].name + "\r\n";
+      }
+      res.send(answer);
+    });
+    db.close();
   })
 }
